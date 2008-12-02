fa1bd896398cb65aa9010af10065a8fc928ca4c7
couannette <couannette>
2008-12-02 14:25:23 +0000

    template

--- src/lib/system.h
+++ src/lib/system.h
@@ -1,21 +1,31 @@
-/*******************************************************************
- *
- * FreeX3D support library
- *
- * internal header - system.h
- *
- * Library system dependencies.
- *
- * $Id: system.h,v 1.4 2008/11/27 00:27:17 couannette Exp $
- *
- *******************************************************************/
+/*
+=INSERT_TEMPLATE_HERE=
+
+$Id: system.h,v 1.5 2008/12/02 14:25:23 couannette Exp $
+
+FreeX3D support library.
+Internal header: system dependencies.
+
+*/
 
 #ifndef __LIBFREEX3D_SYSTEM_H__
 #define __LIBFREEX3D_SYSTEM_H__
 
+/**
+ * Strict necessary common system header files:
+ *
+ * try to remove as much include as possible from here...
+ *
+ * for modularity some headers are defined for special needs:
+ *
+ * system_net.h all network related headers
+ * display.h    all Aqua/X11 related headers
+ */
+
 #if HAVE_STDINT_H
 # include <stdint.h>
 #endif
+
 
 #if STDC_HEADERS
 # include <stdio.h>
@@ -107,6 +117,7 @@ typedef unsigned char _Bool;
 
 #include <netinet/in.h>
 #include <sys/socket.h>
+
 
 
 #endif /* __LIBFREEX3D_SYSTEM_H__ */
