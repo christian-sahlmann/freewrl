23cc6b2689c390491a91450f011f238fd4ef2126
istakenv <istakenv>
2009-06-02 21:30:56 +0000

    changed ifdef of OPENGL_VERSION_2_0 to GL_VERSION_2_0

--- src/lib/scenegraph/Component_ProgrammableShaders.c
+++ src/lib/scenegraph/Component_ProgrammableShaders.c
@@ -1,7 +1,7 @@
 /*
 =INSERT_TEMPLATE_HERE=
 
-$Id: Component_ProgrammableShaders.c,v 1.10 2009/05/29 20:57:47 crc_canada Exp $
+$Id: Component_ProgrammableShaders.c,v 1.11 2009/06/02 21:30:56 istakenv Exp $
 
 X3D Programmable Shaders Component
 
@@ -213,7 +213,7 @@ static void shaderErrorLog(GLuint myShader) {
 			} \
 		} 
 
-#ifdef OPENGL_VERSION_2_0
+#ifdef GL_VERSION_2_0
 	#define LINK_IF_VALID \
 		if (node->isValid) { \
 			/* link the shader programs together */ \
