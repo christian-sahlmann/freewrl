173c8c1ca08c0eb03cc25a2d5af21bc266f3d68d
sdumoulin <sdumoulin>
2009-08-07 14:41:06 +0000

    Changed for Doug Sanden's Swig

--- src/libeai/EAI_C_Advise.c
+++ src/libeai/EAI_C_Advise.c
@@ -1,8 +1,17 @@
 #ifndef AQUA
+#ifndef WIN32
 #include "config.h"
 #include "system.h"
 #endif
+#endif
 #include "EAI_C.h"
+#ifdef WIN32
+#include "../lib/vrml_parser/CParseGeneral.h" /*for anyVrml */
+X3DNode *_swigNewMF(int itype, int num );
+X3DNode *X3D_newSF(int nodetype);
+//extern int haveSwigDirectCB;
+#endif
+
 #define LOCK_ADVISE_TABLE printf ("locking advise table\n");
 #define UNLOCK_ADVISE_TABLE printf ("unlocking advise table\n");
 
@@ -59,6 +68,22 @@ int X3DAdvise (X3DEventOut *node, void *fn) {
 	return AdviseIndex;
 }
 
+union anyVrml* getListenerData(int index)
+{
+        union anyVrml* anynode;
+        LOCK_ADVISE_TABLE
+        anynode = (union anyVrml*)EAI_ListenerTable[index].dataArea;
+        UNLOCK_ADVISE_TABLE
+        return anynode;
+}
+int getListenerType(int index)
+{
+        int type;
+        LOCK_ADVISE_TABLE
+        type = EAI_ListenerTable[index].type;
+        UNLOCK_ADVISE_TABLE
+        return type;
+}
 
 void _handleFreeWRLcallback (char *line) {
 	double evTime;
@@ -107,10 +132,30 @@ void _handleFreeWRLcallback (char *line) {
 			EAI_ListenerTable[count].functionHandler(EAI_ListenerTable[count].dataArea);
 		} else {
 			if (_X3D_FreeWRL_Swig_FD) {
+#ifdef WIN32
+                                char bigbuf[128];
+                                char buf[32];
+
+                                /*
+                                sprintf(bigbuf,"%d ",EAI_ListenerTable[count].FreeWRL_RegisterNumber);
+                                sprintf(buf,"%lf ",evTime);
+                                strcat(bigbuf,buf);
+                                sprintf(buf,"%d ",count);
+                                strcat(bigbuf,buf);
+                                */
+                                /*fetch data from script side with X3DNode* X3D_swigCallbackData(char *ListenerTableIndex) */
+                                sprintf(bigbuf,"%d %lf %d ",EAI_ListenerTable[count].FreeWRL_RegisterNumber,evTime,count);
+                                send(_X3D_FreeWRL_Swig_FD, (char *)&bigbuf[0],strlen(bigbuf),0);
+
+                                /* send(_X3D_FreeWRL_Swig_FD, (const char *) EAI_ListenerTable[count].FreeWRL_RegisterNumber, sizeof(EAI_ListenerTable[count].FreeWRL_RegisterNumber),0); binary int*/
+                                /*send(_X3D_FreeWRL_Swig_FD, (const char *) EAI_ListenerTable[count].dataArea, sizeof(EAI_ListenerTable[count].dataArea),0);*/
+#else
 				char buf[64];
 				sprintf(buf, "%d ", count);
+
                 		write(_X3D_FreeWRL_Swig_FD, buf, strlen(buf));
                 		write(_X3D_FreeWRL_Swig_FD, EAI_ListenerTable[count].dataArea, sizeof(EAI_ListenerTable[count].dataArea));
+#endif
 			} else {
 				printf("no socket connected for callbacks!");
 			}
