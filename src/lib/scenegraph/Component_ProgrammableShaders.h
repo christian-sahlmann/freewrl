18c3ec218883f57e376e4621d716e2fbd8c18ade
crc_canada <crc_canada>
2009-08-08 23:09:43 +0000

    new file

---
+++ src/lib/scenegraph/Component_ProgrammableShaders.h
@@ -0,0 +1,15 @@
+/*
+=INSERT_TEMPLATE_HERE=
+
+$Id: Component_ProgrammableShaders.h,v 1.1 2009/08/08 23:09:43 crc_canada Exp $
+
+Proximity sensor macro.
+
+*/
+
+#ifndef __FREEWRL_SCENEGRAPH_PROGRAMMABLESHADERS_H__
+#define __FREEWRL_SCENEGRAPH_PROGRAMMABLESHADERS_H__
+
+extern GLuint globalCurrentShader;
+
+#endif /* __FREEWRL_SCENEGRAPH_PROGRAMMABLESHADERS_H__ */
