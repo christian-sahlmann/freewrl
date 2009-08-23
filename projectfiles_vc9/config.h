e48ced49e9edf4924272683b271a23c2025f0ac6
dug9 <dug9>
2009-08-23 15:46:48 +0000

    dug9 msvc project files

---
+++ projectfiles_vc9/config.h
@@ -0,0 +1,259 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/*win32 - where do these go? */
+#define TARGET_WIN32
+#undef TARGET_AQUA
+
+/* Path to internet browser */
+#undef BROWSER
+#define BROWSER "C:/Program Files/Mozilla Firefox/firefox.exe"
+
+/* Directory for fonts. */
+#undef FONTS_DIR
+#define FONTS_DIR "C:/source/freewrl/fonts"
+
+/* The FreeWRL message wrapper program name. */
+#undef FREEWRL_MESSAGE_WRAPPER
+#define FREEWRL_MESSAGE_WRAPPER "time"
+
+/* The FreeWRL plugin shared object name. */
+#undef FREEWRL_PLUGIN
+
+/* The FreeWRL program name. */
+#undef FREEWRL_PROGRAM
+#define FREEWRL_PROGRAM freeWrl.exe
+
+/* The FreeWRL sound wrapper program name. */
+#undef FREEWRL_SOUND_MANAGER
+
+/* Define to 1 if you have the <ctype.h> header file. */
+#undef HAVE_CTYPE_H
+
+/* Define to 1 if you have the <dirent.h> header file. */
+#undef HAVE_DIRENT_H
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#undef HAVE_DLFCN_H
+
+/* Define to 1 if you have the <errno.h> header file. */
+#undef HAVE_ERRNO_H
+
+/* Define to 1 if you have the <expat.h> header file. */
+#undef HAVE_EXPAT_H
+#define HAVE_EXPAT_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#undef HAVE_FCNTL_H
+
+/* Define to 1 if you have the `fork' function. */
+#undef HAVE_FORK
+
+/* Define to 1 if you have the `getopt' function. */
+#undef HAVE_GETOPT
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#undef HAVE_GETOPT_H
+#define HAVE_GETOPT_H 1
+
+/* Define to 1 if you have the `getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
+
+/* Define to 1 if you have the `getopt_long_only' function. */
+#undef HAVE_GETOPT_LONG_ONLY
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#undef HAVE_INTTYPES_H
+/* there a
+
+/* Define to 1 if you have the <jpeglib.h> header file. */
+#undef HAVE_JPEGLIB_H
+#define HAVE_JPEGLIB_H 1
+
+/* Define to 1 if you have the <X11/extensions/Print.h> header file. */
+#undef HAVE_LIB_XP
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#undef HAVE_MALLOC
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <math.h> header file. */
+#undef HAVE_MATH_H
+#define HAVE_MATH_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have Motif. */
+#undef HAVE_MOTIF
+
+/* Define if you have POSIX threads libraries and header files. */
+#undef HAVE_PTHREAD
+#define HAVE_PTHREAD 1
+
+/* Define to 1 if you have the <sched.h> header file. */
+#undef HAVE_SCHED_H
+
+/* Define to 1 if you have the <signal.h> header file. */
+#undef HAVE_SIGNAL_H
+#define HAVE_SIGNAL_H 1
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#undef HAVE_STDBOOL_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#undef HAVE_STDIO_H
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strchr' function. */
+#undef HAVE_STRCHR
+#define HAVE_STRCHR 1
+/* Define to 1 if you have the <strings.h> header file. */
+#undef HAVE_STRINGS_H
+/* #define HAVE_STRINGS_H 1 WIN32 HAS string.h */
+
+/* Define to 1 if you have the <string.h> header file. */
+#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strrchr' function. */
+#undef HAVE_STRRCHR
+#define HAVE_STRRCHR 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#undef HAVE_SYS_IOCTL_H
+
+/* Define to 1 if you have the <sys/ipc.h> header file. */
+#undef HAVE_SYS_IPC_H
+
+/* Define to 1 if you have the <sys/msg.h> header file. */
+#undef HAVE_SYS_MSG_H
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#undef HAVE_SYS_SOCKET_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#undef HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#undef HAVE_SYS_TIME_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/wait.h> header file. */
+#undef HAVE_SYS_WAIT_H
+
+/* Define to 1 if you have the <time.h> header file. */
+#undef HAVE_TIME_H
+#define HAVE_TIME_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#undef HAVE_UNISTD_H
+
+/* Define to 1 if you have the `vfork' function. */
+#undef HAVE_VFORK
+
+/* Define to 1 if you have the <vfork.h> header file. */
+#undef HAVE_VFORK_H
+
+/* Define to 1 if `fork' works. */
+#undef HAVE_WORKING_FORK
+
+/* Define to 1 if `vfork' works. */
+#undef HAVE_WORKING_VFORK
+
+/* Define to 1 if you have the <X11/extensions/xf86vmode.h> header file. */
+#undef HAVE_X11_EXTENSIONS_XF86VMODE_H
+
+/* Define if you have the Xxf86vm library. */
+#undef HAVE_XF86_VMODE
+
+/* Define to 1 if the system has the type `_Bool'. */
+#undef HAVE__BOOL
+
+/* Path to image converter. set to NULL if none */
+#undef IMAGECONVERT
+#define IMAGECONVERT NULL
+
+/* Name of package */
+#undef PACKAGE
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT " freewrl "
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#undef PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the version of this package. */
+#undef PACKAGE_VERSION
+
+/* Directory where to install browser plugin. */
+#undef PLUGIN_DIR
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+#undef PTHREAD_CREATE_JOINABLE
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#undef RETSIGTYPE
+
+/* Path to sound converter */
+#undef SOUNDCONV
+
+/* Define to 1 if you have the ANSI C header files. */
+#undef STDC_HEADERS
+#define STDC_HEADERS 1
+
+/* Path to zip decompressor */
+#undef UNZIP
+#define UNZIP "C:/Program Files/GnuWin32/bin"
+
+/* Version number of package */
+#undef VERSION
+
+/* Path to http fetcher */
+#undef WGET
+#define WGET "C:/Program Files/GnuWin32/bin"
+
+/* Define to 1 if the X Window System is missing or not being used. */
+#undef X_DISPLAY_MISSING
+#define X_DISPLAY_MISSING 1
+
+/* enable debugging (default=off) */
+#undef _DEBUG
+
+/* Define to the type of a signed integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+#undef int32_t
+#define int32_t long
+
+/* Define to rpl_malloc if the replacement function should be used. */
+#undef malloc
+
+/* Define to `int' if <sys/types.h> does not define. */
+#undef pid_t
+
+/* Define as `fork' if `vfork' does not work. */
+#undef vfork
+#define vfork fork
+
+#undef HAVE_DIRECT_H
+#define HAVE_DIRECT_H 1
