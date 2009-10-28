c1c2f9e6611398782bf9db40e8c19f49c6c3ea91
crc_canada <crc_canada>
2009-10-28 18:30:56 +0000

    builds now in the appleOSX directory.

--- appleOSX/buildi386.sh
+++ appleOSX/buildi386.sh
@@ -2,16 +2,16 @@
 
 # Make i386 library first
 make distclean
-cp -f ../freewrl/JS/js1.5/src/config/Darwin.mk.i386 ../freewrl/JS/js1.5/src/config/Darwin.mk
-cp -f ../freewrl/JS/js1.5/src/editline/Makefile.ref.i386 ../freewrl/JS/js1.5/src/editline/Makefile.ref
-cp -f ../freewrl/JS/js1.5/src/Makefile.ref.uni ../freewrl/JS/js1.5/src/Makefile.ref
-cp -f ../freewrl/JS/js1.5/src/Makefile.in.uni ../freewrl/JS/js1.5/src/Makefile.in
-cp -f ../freewrl/JS/js1.5/src/rules.mk.uni ../freewrl/JS/js1.5/src/rules.mk
+cp -f ../../freewrl/JS/js1.5/src/config/Darwin.mk.i386 ../../freewrl/JS/js1.5/src/config/Darwin.mk
+cp -f ../../freewrl/JS/js1.5/src/editline/Makefile.ref.i386 ../../freewrl/JS/js1.5/src/editline/Makefile.ref
+cp -f ../../freewrl/JS/js1.5/src/Makefile.ref.uni ../../freewrl/JS/js1.5/src/Makefile.ref
+cp -f ../../freewrl/JS/js1.5/src/Makefile.in.uni ../../freewrl/JS/js1.5/src/Makefile.in
+cp -f ../../freewrl/JS/js1.5/src/rules.mk.uni ../../freewrl/JS/js1.5/src/rules.mk
 cp -f vrml.conf.i386 vrml.conf.aqua
 perl Makefile.PL.osx
 make install
 cp -f libFreeWRLFunc.dylib VRMLFunc_i386.dylib
-cp -f ../freewrl/JS/js1.5/src/Darwin_OPT.OBJ/libFreeWRLjs.so libFreeWRLjs_i386.so
+cp -f ../../freewrl/JS/js1.5/src/Darwin_OPT.OBJ/libFreeWRLjs.so libFreeWRLjs_i386.so
 
 # Glue the two libraries together and make sure the library has a good name
 lipo -create VRMLFunc_i386.dylib -output libFreeWRLFunc.dylib
