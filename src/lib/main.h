98b5dc404d6089fbf8ce6486840947165c1b4a3d
crc_canada <crc_canada>
2009-11-23 20:40:57 +0000

    function prototype as required by OSX front end.

---
+++ src/lib/main.h
@@ -0,0 +1,34 @@
+/*
+  $Id: main.h,v 1.1 2009/11/23 20:40:57 crc_canada Exp $
+
+  FreeWRL support library.
+
+*/
+
+/****************************************************************************
+    This file is part of the FreeWRL/FreeX3D Distribution.
+
+    Copyright 2009 CRC Canada. (http://www.crc.gc.ca)
+
+    FreeWRL/FreeX3D is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FreeWRL/FreeX3D is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FreeWRL/FreeX3D.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
+
+
+#ifndef __LIBFREEWRL_SRC_MAIN_H__
+#define __LIBFREEWRL_SRC_MAIN_H__
+
+void OSX_initializeParameters(const char* initialURL);
+
+#endif /* __LIBFREEWRL_SRC_MAIN_H__ */
