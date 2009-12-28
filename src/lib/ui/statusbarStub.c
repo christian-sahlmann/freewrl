7b9b83af78f2525e018bacab97a405e9f18eafeb
dug9 <dug9>
2009-12-28 02:55:21 +0000

    dug9 alternate HUD/statusbar/buttons implementations

---
+++ src/lib/ui/statusbarStub.c
@@ -0,0 +1,48 @@
+/*
+  $Id: statusbarStub.c,v 1.1 2009/12/28 02:55:21 dug9 Exp $
+
+*/
+
+/****************************************************************************
+    This file is part of the FreeWRL/FreeX3D Distribution.
+
+    Copyright 2009 CRC Canada. (http://www.crc.gc.ca)
+
+    FreeWRL/FreeX3D is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FreeWRL/FreeX3D is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FreeWRL/FreeX3D.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
+
+
+void kill_status (void) {
+}
+void update_status(char* msg)
+{
+}
+void hudSetConsoleMessage(char *buffer){}
+void handleButtonOver(){}
+void handleOptionPress(){}
+void handleButtonPress(){}
+
+void setMenuButton_collision(int val){}
+void setMenuButton_texSize(int size){}
+void setMenuButton_headlight(int val){}
+void setMenuButton_navModes(int type){}
+void setMenuStatus(char *stat) {}
+void setMenuFps (float fps) {}
+
+int handleStatusbarHud(int mev, int* clipplane)
+{return 0;}
+void drawStatusBar()
+{
+}
