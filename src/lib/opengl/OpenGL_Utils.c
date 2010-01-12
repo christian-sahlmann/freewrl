c28ef7bbef70fef5f8e59614e55806334355f624
couannette <couannette>
2010-01-12 01:25:43 +0000

    Fixed a little compile error under Linux (does this still work on Mac ?)

--- src/lib/opengl/OpenGL_Utils.c
+++ src/lib/opengl/OpenGL_Utils.c
@@ -1,5 +1,5 @@
 /*
-  $Id: OpenGL_Utils.c,v 1.85 2010/01/08 19:43:05 crc_canada Exp $
+  $Id: OpenGL_Utils.c,v 1.86 2010/01/12 01:25:43 couannette Exp $
 
   FreeWRL support library.
   OpenGL initialization and functions. Rendering functions.
@@ -455,9 +455,6 @@ bool initialize_GL()
         float dif[] = { 1.0, 1.0, 1.0, 1.0 };
         float shin[] = { 0.6, 0.6, 0.6, 1.0 };
         float As[] = { 0.0, 0.0, 0.0, 1.0 };
-	int checktexsize;
-	GLenum err;
-
 
 #if defined (TARGET_AQUA)
 #ifdef OLDCODE
@@ -828,8 +825,9 @@ printf ("kill 12\n");
 	setMenuStatus("NONE");
 
 printf ("kill 13\n");
+#ifdef TARGET_AQUA
 	myglobalContext = NULL; 
-
+#endif
 
 }
 
