a23c941cb63c1c404b5d5443e6b4cb6e78267594
crc_canada <crc_canada>
2010-02-12 20:14:30 +0000

    build for 32 and 64 bit, but no PPC.

---
+++ appleOSX/buildintel.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#build javascript
+cd ../../freewrl/JS/js1.8/src
+./build64_32_noppc.sh
+cp ../../freewrl/JS/js1.8/src/Darwin_DBG.OBJ/libjs.dylib /usr/local/lib/libjs.dylib 
+
+cd ../../../../freex3d/appleOSX/
+
+## Make i386 library first
+make distclean
+cp -f vrml.conf.i386 vrml.conf.aqua
+perl Makefile.PL.osx
+make install
+cp -f libFreeWRLFunc.dylib VRMLFunc_i386.dylib
+
+## Clean and make x86_64 library
+make distclean
+cp -f vrml.conf.x86_64 vrml.conf.aqua
+perl Makefile.PL.osx
+make install
+cp -f libFreeWRLFunc.dylib VRMLFunc_x86_64.dylib
+
+## Glue the two libraries together and make sure the library has a good name
+lipo -create VRMLFunc_i386.dylib VRMLFunc_x86_64.dylib -output libFreeWRLFunc.dylib
+install_name_tool -id libFreeWRLFunc.dylib libFreeWRLFunc.dylib
+cp libFreeWRLFunc.dylib /usr/local/lib/libFreeWRLFunc.dylib
+
+
+## Clean up
+#rm -f VRMLFunc_i386.dylib
+#rm -f VRMLFunc_x86_64.dylib
