8df4b985c5c11f59b2bea966f1013495df4cb7dc
couannette <couannette>
2010-03-29 00:57:24 +0000

    If this is acceptable for IPHONE this is good for Linux :). Seriously: this makes freewrl compile. Maybe some tweak for iPhone is needed (or give me more info).

--- src/lib/display.h
+++ src/lib/display.h
@@ -1,5 +1,5 @@
 /*
-  $Id: display.h,v 1.77 2010/03/28 17:41:12 crc_canada Exp $
+  $Id: display.h,v 1.78 2010/03/29 00:57:24 couannette Exp $
 
   FreeWRL support library.
   Display global definitions for all architectures.
@@ -334,6 +334,7 @@ extern GLenum _global_gl_err;
                                               } \
                                            } 
 #endif
+#define GL_ERROR_MSG  ((char*) gluErrorString(glGetError()))
 
 void resetGeometry();
 void setScreenDim(int wi, int he);
@@ -743,7 +744,6 @@ void setScreenDim(int wi, int he);
 	#define FW_GL_GETDOUBLEV(aaa,bbb)  printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
 	#define FW_GL_LOAD_IDENTITY(aaa) printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
 	#define FW_GL_MATRIX_MODE(aaa)  printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
-	#define GL_ERROR_MSG  printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
 	#define PRINT_GL_ERROR_IF_ANY(aaa) printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
 	#define FW_GL_PIXELSTOREI(aaa,bbb) printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
 	#define FW_GL_SHADEMODEL(aaa) printf ("subbed openglES call at %s:%d \n",__FILE__,__LINE__)
