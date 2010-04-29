b0f942da9f4896d4a71968073fbd6e53f980dae7
crc_canada <crc_canada>
2010-04-29 14:38:33 +0000

    change versions to 1.22.8

--- projectfiles_vc9/fwVersion.c
+++ projectfiles_vc9/fwVersion.c
@@ -1,8 +1,8 @@
     char * libFreeWRL_get_version()
 	{
-		return "libfreeWRL version 1.22.7";
+		return "libfreeWRL version 1.22.8";
 	}
     char * freewrl_get_version()
 	{
-		return "freeWRL version 1.22.7";
+		return "freeWRL version 1.22.8";
 	}
