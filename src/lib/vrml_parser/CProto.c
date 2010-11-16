cd37a0b75d8f41879ef2afac03f31c4a05e60d53
crc_canada <crc_canada>
2010-11-16 15:18:50 +0000

    segfault in proto expansion when an invalid node was specified fixed.

--- src/lib/vrml_parser/CProto.c
+++ src/lib/vrml_parser/CProto.c
@@ -1,7 +1,7 @@
 /*
 =INSERT_TEMPLATE_HERE=
 
-$Id: CProto.c,v 1.48 2010/11/15 00:52:05 crc_canada Exp $
+$Id: CProto.c,v 1.49 2010/11/16 15:18:50 crc_canada Exp $
 
 CProto ???
 
@@ -1413,7 +1413,8 @@ char *protoExpand (struct VRMLParser *me, indexT nodeTypeU, struct ProtoDefiniti
 						APPEND_SPACE
 						APPEND_ISVALUE
 
-					} else if (lastNode->isNODE == NODE_Script) {
+					} else if (lastNode != NULL) {
+						if (lastNode->isNODE == NODE_Script) {
 						#ifdef XXX
 						PROTO_CAT ( "# at G\n");
 						#endif
@@ -1421,6 +1422,7 @@ char *protoExpand (struct VRMLParser *me, indexT nodeTypeU, struct ProtoDefiniti
 						/* Script nodes NEED the fieldname, even if it is blank, so... */
 						APPEND_STRINGTOKEN
 						APPEND_SPACE
+						}
 					}
 
 					i+=2; /* skip the IS and the field */
