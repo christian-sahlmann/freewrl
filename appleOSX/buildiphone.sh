158c2e2ce94fc0a4303702691f2dc4b81deef8a8
crc_canada <crc_canada>
2011-02-15 16:02:36 +0000

    more iPhone changes

--- appleOSX/buildiphone.sh
+++ appleOSX/buildiphone.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 make distclean
-cp -f vrml.conf.iphone vrml.conf.aqua
 perl Makefile.PLStatic.osx
 make install
 
-# Glue the two libraries together and make sure the library has a good name
-install_name_tool -id libFreeWRLFunc.dylib libFreeWRLFunc.dylib
-cp libFreeWRLFunc.dylib /usr/local/lib/libFreeWRLFunc.dylib
-
-# Clean up
-#rm -f libFreeWRLFunc.dylib
--- src/lib/main/MainLoop.c
+++ src/lib/main/MainLoop.c
@@ -1,5 +1,5 @@
 /*
-  $Id: MainLoop.c,v 1.160 2011/02/14 21:52:59 crc_canada Exp $
+  $Id: MainLoop.c,v 1.161 2011/02/15 16:02:36 crc_canada Exp $
 
   FreeWRL support library.
   Main loop : handle events, ...
@@ -288,14 +288,14 @@ __inline double Time1970sec()
 
 
 /* Main eventloop for FreeWRL!!! */
-#ifndef IPHONE
 void EventLoop() {
         static int loop_count = 0;
         static int slowloop_count = 0;
 
 #if defined(TARGET_X11) || defined(TARGET_MOTIF)
         Cursor cursor;
-#endif
+#endif /* TARGET_X11 or TARGET_MOTIF */
+
 	while (refreshOK) {
 		usleep(50);
 	}
@@ -325,7 +325,7 @@ void EventLoop() {
                if (waitsec < 0.005) {
                        usleep(10000);
 		}
-#endif
+#endif /* IPHONE */
         }
 
         /* Set the timestamp */
@@ -335,7 +335,7 @@ void EventLoop() {
         /* any scripts to do?? */
 #ifdef _MSC_VER
 		if(doEvents)
-#endif
+#endif /* _MSC_VER */
 
 	#ifdef HAVE_JAVASCRIPT
         INITIALIZE_ANY_SCRIPTS;
@@ -381,7 +381,8 @@ void EventLoop() {
 	} else {
 		//deactivate_picksensors() ;
 	}
-	#endif
+	#endif /* USE_OSC */
+
 	slowloop_count++ ;
 
         /* handle any events provided on the command line - Robert Sim */
@@ -396,7 +397,8 @@ void EventLoop() {
 
 				DEBUG_XEV("CMD LINE GEN EVENT: %c\n", *keypress_string);
                                 do_keyPress(*keypress_string,KeyPress);
-#endif
+#endif /* NOT AQUA and NOT WIN32 */
+
                                 keypress_string++;
                         } else {
                                 keypress_string=NULL;
@@ -416,7 +418,7 @@ void EventLoop() {
 	    DEBUG_XEV("EVENT through XNextEvent\n");
 	    handle_Xevents(event);
 	}
-#endif
+#endif /* TARGET_X11 */
 
 #if defined(TARGET_MOTIF)
 	/* any updates to the menu buttons? Because of Linux threading
@@ -448,34 +450,22 @@ void EventLoop() {
 			  bev->window, bev->state);
 		break;
 	    }
-#endif //XEVENT_VERBOSE
+#endif /* XEVENT_VERBOSE */
 
-#if 0 // no need for this hack now that events are correctly propagated through Motif
-	    /**
-	     *   Quick hack to make events propagate
-	     *   to the drawing area.... :)
-	     */
-	    if (aev->window == GLwin) {
-		handle_Xevents(event);
-	    } else {
-		XtDispatchEvent (&event);
-	    }
-#else // clean implementation
 	    DEBUG_XEV("EVENT through XtDispatchEvent\n");
 	    XtDispatchEvent (&event);
-#endif
 	}
 
-#endif //defined(TARGET_MOTIF)
+#endif /* TARGET_MOTIF */
 
-#if defined(_MSC_VER) //TARGET_WIN32)
+#if defined(_MSC_VER) 
 	/**
 	 *   Win32 event loop
 	 *   gives windows message handler a time slice and 
 	 *   it calls handle_aqua and do_keypress from fwWindow32.c 
 	 */
 	doEventsWin32A(); 
-#endif
+#endif /* _MSC_VER */
 
         /* Viewer move viewpoint */
         handle_tick();
@@ -531,7 +521,7 @@ void EventLoop() {
 			printf("COS %d (%s)\n",
 			       (unsigned int) CursorOverSensitive,
 			       stringNodeType(CursorOverSensitive->_nodeType));
-                #endif
+                #endif /* VERBOSE */
 
                 /* did we have a click of button 1? */
 
@@ -608,7 +598,8 @@ void EventLoop() {
                         ocurse = ccurse;
                         setAquaCursor(ccurse);
                 }
-#endif
+#endif /* CURSOR CHANGING CODE */
+
         } /* (!NavigationMode && HaveSensitive) */
 
 
@@ -645,153 +636,8 @@ void EventLoop() {
 		askForRefresh = FALSE;
 	}
 }
-#else
-GLuint LoadShader ( GLenum type, const char *shaderSrc )
-{
-   GLuint shader;
-   GLint compiled;
-   
-   // Create the shader object
-   shader = glCreateShader ( type );
 
-   if ( shader == 0 )
-   	return 0;
 
-   // Load the shader source
-   glShaderSource ( shader, 1, &shaderSrc, NULL );
-   
-   // Compile the shader
-   glCompileShader ( shader );
-
-   // Check the compile status
-   glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled );
-
-   if ( !compiled ) 
-   {
-      GLint infoLen = 0;
-
-      glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
-      
-      if ( infoLen > 1 )
-      {
-         char* infoLog = malloc (sizeof(char) * infoLen );
-
-         glGetShaderInfoLog ( shader, infoLen, NULL, infoLog );
-         
-         free ( infoLog );
-      }
-
-      glDeleteShader ( shader );
-      return 0;
-   }
-
-   return shader;
-
-}
-
-///
-// Initialize the shader and program object
-//
-int Init ( ESContext *esContext )
-{
-   esContext->userData = malloc(sizeof(UserData));
-
-   UserData *userData = esContext->userData;
-   GLbyte vShaderStr[] =  
-      "attribute vec4 vPosition;    \n"
-      "void main()                  \n"
-      "{                            \n"
-      "   gl_Position = vPosition;  \n"
-      "}                            \n";
-   
-   GLbyte fShaderStr[] =  
-      "precision mediump float;\n"\
-      "void main()                                  \n"
-      "{                                            \n"
-      "  gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n"
-      "}                                            \n";
-
-   GLuint vertexShader;
-   GLuint fragmentShader;
-   GLuint programObject;
-   GLint linked;
-
-   // Load the vertex/fragment shaders
-   vertexShader = LoadShader ( GL_VERTEX_SHADER, vShaderStr );
-   fragmentShader = LoadShader ( GL_FRAGMENT_SHADER, fShaderStr );
-
-   // Create the program object
-   programObject = glCreateProgram ( );
-   
-   if ( programObject == 0 )
-      return 0;
-
-   glAttachShader ( programObject, vertexShader );
-   glAttachShader ( programObject, fragmentShader );
-
-   // Bind vPosition to attribute 0   
-   glBindAttribLocation ( programObject, 0, "vPosition" );
-
-   // Link the program
-   glLinkProgram ( programObject );
-
-   // Check the link status
-   glGetProgramiv ( programObject, GL_LINK_STATUS, &linked );
-
-   if ( !linked ) 
-   {
-      GLint infoLen = 0;
-
-      glGetProgramiv ( programObject, GL_INFO_LOG_LENGTH, &infoLen );
-      
-      if ( infoLen > 1 )
-      {
-         char* infoLog = malloc (sizeof(char) * infoLen );
-
-         glGetProgramInfoLog ( programObject, infoLen, NULL, infoLog );
-         
-         free ( infoLog );
-      }
-
-      glDeleteProgram ( programObject );
-      return GL_FALSE;
-   }
-
-   // Store the program object
-   userData->programObject = programObject;
-
-   glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
-   return GL_TRUE;
-}
-
-///
-// Draw a triangle using the shader pair created in Init()
-//
-void EventLoop() {}
-void Draw ( ESContext *esContext )
-{
-   UserData *userData = esContext->userData;
-   GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f, 
-                           -0.5f, -0.5f, 0.0f,
-                            0.5f, -0.5f, 0.0f };
-      
-   // Set the viewport
-   glViewport ( 0, 0, esContext->width, esContext->height );
-   
-   // Clear the color buffer
-   glClear ( GL_COLOR_BUFFER_BIT );
-
-   // Use the program object
-   glUseProgram ( userData->programObject );
-
-   // Load the vertex data
-   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
-   glEnableVertexAttribArray ( 0 );
-
-   glDrawArrays ( GL_TRIANGLES, 0, 3 );
-}
-
-#endif
 #if !defined( AQUA ) && !defined( WIN32 )
 void handle_Xevents(XEvent event) {
 
