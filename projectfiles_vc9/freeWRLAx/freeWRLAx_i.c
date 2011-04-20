be902f912df8634c749e15bde5dcf9c8977f2bb0
dug9 <dug9>
2011-04-20 23:35:23 +0000

    dug9 did I forget to commit these?

--- projectfiles_vc9/freeWRLAx/freeWRLAx_i.c
+++ projectfiles_vc9/freeWRLAx/freeWRLAx_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 6.00.0361 */
-/* at Sun Apr 17 16:19:17 2011
+/* at Sun Apr 17 16:48:08 2011
  */
 /* Compiler settings for .\freeWRLAx.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
