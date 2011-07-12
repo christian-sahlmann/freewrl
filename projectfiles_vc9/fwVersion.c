15e72853d87778569e571f10f41691c18aa59b9c
dug9 <dug9>
2011-07-12 17:49:40 +0000

    dug9 1.22.12

--- projectfiles_vc9/fwVersion.c
+++ projectfiles_vc9/fwVersion.c
@@ -1,8 +1,8 @@
     char * libFreeWRL_get_version()
 	{
-		return "libfreeWRL version 1.22.11";
+		return "libfreeWRL version 1.22.12";
 	}
     char * freewrl_get_version()
 	{
-		return "freeWRL version 1.22.11";
+		return "freeWRL version 1.22.12";
 	}
