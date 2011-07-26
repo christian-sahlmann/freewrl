1a1ac28f19c6e8f41edaf4493f39ac92e538a32c
dug9 <dug9>
2011-07-26 19:51:16 +0000

    dug9 - put Reload button back on

--- src/lib/ui/statusbarHud.c
+++ src/lib/ui/statusbarHud.c
@@ -1,5 +1,5 @@
 /*
-  $Id: statusbarHud.c,v 1.40 2011/07/25 19:30:11 dug9 Exp $
+  $Id: statusbarHud.c,v 1.41 2011/07/26 19:51:16 dug9 Exp $
 
 */
 
@@ -869,7 +869,7 @@ void statusbar_init(struct tstatusbar *t){
 		p->hadString = 0;
 
 		p->showButtons =0;
-		p->nbuts = mbuts -1; // leave Reload out of pre2
+		p->nbuts = mbuts;// -1; // leave Reload out of pre2
 		//p->butrect[4][mbuts];
 		//p->butStatus[mbuts];
 		//p->butts[mbuts][2];
