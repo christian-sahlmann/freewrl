c2819772735ae3ef1a49aa5709a9ab066062052f
dug9 <dug9>
2011-07-31 19:52:52 +0000

    dug9 MS has a file:/// as well as file:// path prefix.

--- src/lib/input/InputFunctions.c
+++ src/lib/input/InputFunctions.c
@@ -1,5 +1,5 @@
 /*
-  $Id: InputFunctions.c,v 1.18 2010/08/11 16:43:32 crc_canada Exp $
+  $Id: InputFunctions.c,v 1.19 2011/07/31 19:52:52 dug9 Exp $
 
   FreeWRL support library.
   Input functions (EAI, mouse, keyboard, ...).
@@ -56,10 +56,12 @@ char * stripLocalFileName (char * origName)
 
 	/* remove whitespace, etc */
 	while ((*origName != '\0') && (*origName <= ' ')) origName++;
+        if ((strncmp(origName,"file:///", strlen("file:///"))== 0)) // MS windows: file:///C:/source2/freewrl/freex3d/projectfiles_vc9/testAx/1.x3d
+			origName += strlen ("file:///");
 
         if ((strncmp(origName,"file://", strlen("file://"))== 0) || 
-	    (strncmp(origName,"FILE://", strlen("FILE://"))== 0)) {
-		origName += strlen ("FILE://");
+			(strncmp(origName,"FILE://", strlen("FILE://"))== 0)) {
+				origName += strlen ("FILE://");
 		return origName;
 	}
 	return origName;
